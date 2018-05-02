b749dc62271a7ddc8877a69b439ff28dc5bd9880
prashantnaik <prashantnaik@outlook.com>
2018-05-02 21:03:23 +0100

    Breaking up tests using blocks

--- src/test/groovy/FirstSpecification.groovy
+++ src/test/groovy/FirstSpecification.groovy
@@ -5,4 +5,18 @@ class FirstSpecification extends Specification {
         expect:
         1 + 1 == 2
     }
+
+    def "two plus two equal to four"()
+    {
+        given:
+            int left = 2;
+            int right =2;
+
+        when:
+            int result = left+right
+
+        then:
+            result==4
+
+    }
 }
